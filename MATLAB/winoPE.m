72ce19ce1206281a327c45689361eda688f5bac0
ZiyangNing <callmejasonbro@gmail.com>
2024-11-06 15:34:35 -0500

    need to add loop for input

--- MATLAB/winoPE.m
+++ MATLAB/winoPE.m
@@ -1,9 +1,9 @@
 % out_U and out_V will be the same (n,r) as the in_U and in_V
-% middle_n and middle_r are the (m,r) used for A_T
+% middle_n and middle_r are the (n,r) used for A_T
 
 function [out_U, out_V, Y] = winoPE(in_U, in_V, size, input_n, input_r, middle_n, middle_r, Y_n, Y_r)
 % if size is 0, 3*3 filter
-% if size is 1, 5*5 filter
+% if size is 1, 1*1 filter
     
     in_U = fi(in_U, 1, input_n, input_r);
     in_V = fi(in_V, 1, input_n, input_r);
@@ -14,13 +14,18 @@ function [out_U, out_V, Y] = winoPE(in_U, in_V, size, input_n, input_r, middle_n
         s0 = 0;
         s1 = 1;
     elseif (size == 1)
-        s0 = 1;
+        s0 = 0;
         s1 = 0;
     end
+    
     A_T = [1, 1, 1, 1, 1, 0;
-           0, 1,-1, 2, -1, s0;
+           0, 1,-1, 2, -2, s0;
            0, 1, 1, 4, 4, 0;
            0, 1, -1,8,-8, s1];
+       
+    if (size == 1)
+        A_T = A_T(1:2, :);
+    end
        
    A_T = fi(A_T, 1, middle_n, middle_r);
    
@@ -28,12 +33,3 @@ function [out_U, out_V, Y] = winoPE(in_U, in_V, size, input_n, input_r, middle_n
    Y = fi(Y, 1, Y_n, Y_r);
 
 end
-
-
-%     G = [1/4,   0,      0;
-%         -1/6,  -1/6,    -1/6;
-%         -1/6,   1/6,    -1/6;
-%         1/24,   1/12,   1/6;
-%         1/24,   -1/12,  1/6;
-%         1/24,   -1/12,  1/6;
-%         s0,     0,      s1];
--- MATLAB/winoPE_test.m
+++ MATLAB/winoPE_test.m
@@ -1,22 +1,83 @@
-%% setup input
+%% Test Single PE functionality 
+clear;
+clc;
+% clf;
 
-filter_1 = 2;
-filter_3 = [1, 0, 0; 0, 1, 0; 0, 0, 1];
+size = 0;       %size = 0 for 3*3, = 1 for 1*1
 
-input_m = 8;
-input_n = 4;
-middle_m = 12;
-middle_n = 4;
-out_m = 8;
-out_n = 4;
+% should be values from -1 to 1?
+if (size == 0)
+    % TODO: CHANGE YOUR kernel HERE
+    kernel =    [-1, -1, -1; 
+                 -1,  8, -1; 
+                 -1, -1, -1];   %Edge detection kernel
+    s0 = 0;
+    s1 = 1;
+elseif (size == 1)
+     % TODO: CHANGE YOUR kernel HERE
+     kernel = [0.3];
+     s0 = 1;
+     s1 = 0;
+end
 
-A = imread('test598.jpg');
-A_red = A(:, 1);
-imshow(A);
+% G matrix pre-calc
+G = [1/4,   0,      0;
+    -1/6,  -1/6,    -1/6;
+    -1/6,   1/6,    -1/6;
+    1/24,   1/12,   1/6;
+    1/24,   -1/12,  1/6;
+    1/24,   -1/12,  1/6;
+    s0,     0,      s1];
 
-out_1 = conv2(A, filter_1);
-out_3 = conv2(A, filter_3);
+B_T = [4, 0, -5, 0, 1, 0;
+       0, -4, -4, 1, 1, 0;
+       0, 4, -4, -1, 1, 0;
+       0, -2, -1, 2, 1, 0;
+       0, 2, -1, -2, 1, 0;
+       0, 4, 0, -5, 0, 1];
 
-figure();
+if (size == 1)
+    G = G(:, 1);
+end
+        
+%input and output size
+input_n = 8;
+input_r = 4;
+middle_n = 12;
+middle_r = 4;
+out_n = 12;
+out_r = 4;
 
-imshow(out_1);
+%extract data from image
+A = imread('test2.jpg');
+A_red = A(:, :, 1);
+A_green = A(:, :, 2);
+A_blue = A(:, :, 3);
+
+%normalize each channel 
+%REQUIRED:: Data pre-processing step, data will be between 0 to 1
+A_red_norm = double(A_red) / 255;
+A_green_norm = double(A_green) / 255;
+A_blue_norm = double(A_blue) / 255;
+
+%put kernel and input into fixed point notation
+red_in = fi(A_red_norm, 1, input_n, input_r);
+green_in = fi(A_green_norm, 1, input_n, input_r);
+blue_in = fi(A_blue_norm, 1, input_n, input_r);
+kernel = fi(kernel, 1, input_n, input_r);
+
+%convolution
+red_out = conv2(double(red_in), double(kernel)); %maybe need the 'same'
+green_out = conv2(double(green_in), double(kernel)); %maybe need the 'same'
+blue_out = conv2(double(blue_in), double(kernel)); %maybe need the 'same'
+
+%fixed point output
+red_out_fixed = fi(red_out, 1, out_n, out_r);
+green_out_fixed = fi(green_out, 1, out_n, out_r);
+blue_out_fixed = fi(blue_out, 1, out_n, out_r);
+
+% first approach, mutiply everything in double then convert to fixed
+V = G * kernel * G.';
+U = B_T * input * B_T.';
+
+
