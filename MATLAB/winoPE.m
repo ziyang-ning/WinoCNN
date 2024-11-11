b3224ae9d85d2646933f459b1f331384cc87ff94
ZiyangNing <callmejasonbro@gmail.com>
2024-11-10 20:28:49 -0500

    tested winoPE, correct, might need to tune fixed-point

--- MATLAB/winoPE.m
+++ MATLAB/winoPE.m
@@ -1,19 +1,20 @@
 % out_U and out_V will be the same (n,r) as the in_U and in_V
 % middle_n and middle_r are the (n,r) used for A_T
 
-function [out_U, out_V, Y] = winoPE(in_U, in_V, size, input_n, input_r, middle_n, middle_r, Y_n, Y_r)
-% if size is 0, 3*3 filter
-% if size is 1, 1*1 filter
+function [out_U, out_V, Y] = winoPE(in_U, in_V, size_k, input_n, input_r, middle_n, middle_r, Y_n, Y_r)
+% if size_k is 0, 3*3 filter
+% if size_k is 1, 1*1 filter
     
-    in_U = fi(in_U, 1, input_n, input_r);
-    in_V = fi(in_V, 1, input_n, input_r);
+    in_U = double(fi(in_U, 1, input_n, input_r));
+    in_V = double(fi(in_V, 1, input_n, input_r));
+    
     out_U = in_U;
     out_V = in_V;
     
-    if (size == 0)
+    if (size_k == 0)
         s0 = 0;
         s1 = 1;
-    elseif (size == 1)
+    elseif (size_k == 1)
         s0 = 0;
         s1 = 0;
     end
@@ -23,13 +24,14 @@ function [out_U, out_V, Y] = winoPE(in_U, in_V, size, input_n, input_r, middle_n
            0, 1, 1, 4, 4, 0;
            0, 1, -1,8,-8, s1];
        
-    if (size == 1)
+    if (size_k == 1)
         A_T = A_T(1:2, :);
     end
        
-   A_T = fi(A_T, 1, middle_n, middle_r);
-   
+   A_T = double(fi(A_T, 1, middle_n, middle_r));
+%    UV = fi(double(in_U) .* double(in_V), 1, middle_n, middle_r);
+%    Y = double(A_T) * double(UV) * double(A_T.');
    Y = A_T * (in_U .* in_V) * A_T.';
-   Y = fi(Y, 1, Y_n, Y_r);
+   Y = double(fi(Y, 1, Y_n, Y_r));
 
 end
--- MATLAB/winoPE_test.m
+++ MATLAB/winoPE_test.m
@@ -3,17 +3,20 @@ clear;
 clc;
 % clf;
 
-size = 0;       %size = 0 for 3*3, = 1 for 1*1
+%------------- Setup Params -------------------
+
+size_k = 0;       % size_k = 0 for 3*3, = 1 for 1*1
 
 % should be values from -1 to 1?
-if (size == 0)
+if (size_k == 0)
     % TODO: CHANGE YOUR kernel HERE
+    % NOTE: the kernel will be later normalized to range from [-1 to 1]
     kernel =    [-1, -1, -1; 
                  -1,  8, -1; 
-                 -1, -1, -1];   %Edge detection kernel
+                 -1, -1, -1];   % Edge detection kernel
     s0 = 0;
     s1 = 1;
-elseif (size == 1)
+elseif (size_k == 1)
      % TODO: CHANGE YOUR kernel HERE
      kernel = [0.3];
      s0 = 1;
@@ -26,7 +29,6 @@ G = [1/4,   0,      0;
     -1/6,   1/6,    -1/6;
     1/24,   1/12,   1/6;
     1/24,   -1/12,  1/6;
-    1/24,   -1/12,  1/6;
     s0,     0,      s1];
 
 B_T = [4, 0, -5, 0, 1, 0;
@@ -36,48 +38,141 @@ B_T = [4, 0, -5, 0, 1, 0;
        0, 2, -1, -2, 1, 0;
        0, 4, 0, -5, 0, 1];
 
-if (size == 1)
+if (size_k == 1)
     G = G(:, 1);
 end
         
-%input and output size
-input_n = 8;
-input_r = 4;
+% input and output size
+input_n = 8;    %size for the input matrix and filter
+input_r = 7;
+UV_n = 12;
+UV_r = 7;
 middle_n = 12;
-middle_r = 4;
+middle_r = 7;
 out_n = 12;
-out_r = 4;
+out_r = 7;
 
-%extract data from image
+% extract data from image
 A = imread('test2.jpg');
+%
+% A = ones(64,64,3);  %All white image
+
 A_red = A(:, :, 1);
 A_green = A(:, :, 2);
 A_blue = A(:, :, 3);
 
-%normalize each channel 
-%REQUIRED:: Data pre-processing step, data will be between 0 to 1
+% normalize each channel 
+% REQUIRED:: Data pre-processing step, data will be between 0 to 1
 A_red_norm = double(A_red) / 255;
 A_green_norm = double(A_green) / 255;
 A_blue_norm = double(A_blue) / 255;
 
-%put kernel and input into fixed point notation
+% normalize kernel to [-1, 1]
+max_val = max(abs(kernel(:)));
+kernel_norm = kernel / max_val;
+
+%------------- END OF Setup Params -------------------
+
+
+
+%------------- START OF ALL FLOAT TEST -------------------
+
+red_float_out = conv2(A_red_norm,kernel_norm,'same');
+green_float_out = conv2(A_green_norm,kernel_norm,'same');
+blue_float_out = conv2(A_blue_norm,kernel_norm,'same');
+
+%------------- END OF ALL FLOAT TEST -------------------
+
+
+
+%------------- START OF FIXED GROUND TRUTH -------------------
+%This section won't be very useful
+
+% put kernel and input into fixed point notation
 red_in = fi(A_red_norm, 1, input_n, input_r);
 green_in = fi(A_green_norm, 1, input_n, input_r);
 blue_in = fi(A_blue_norm, 1, input_n, input_r);
-kernel = fi(kernel, 1, input_n, input_r);
+kernel_in = fi(kernel_norm, 1, input_n, input_r);
 
-%convolution
-red_out = conv2(double(red_in), double(kernel)); %maybe need the 'same'
-green_out = conv2(double(green_in), double(kernel)); %maybe need the 'same'
-blue_out = conv2(double(blue_in), double(kernel)); %maybe need the 'same'
+% convolution
+red_out = conv2(double(red_in), double(kernel_in),'same');
+green_out = conv2(double(green_in), double(kernel_in),'same');
+blue_out = conv2(double(blue_in), double(kernel_in),'same');
 
-%fixed point output
+
+% fixed point output
 red_out_fixed = fi(red_out, 1, out_n, out_r);
 green_out_fixed = fi(green_out, 1, out_n, out_r);
 blue_out_fixed = fi(blue_out, 1, out_n, out_r);
 
+%------------- END OF FIXED GROUND TRUTH -------------------
+
+%------------- START OF WINOPE TESTBENCH -------------------
+
+% padding the input
+padSize = floor((size(kernel) - 1) / 2);
+A_red_padded = padarray(A_red_norm, padSize, 0, 'both');
+A_green_padded = padarray(A_green_norm, padSize, 0, 'both');
+A_blue_padded = padarray(A_blue_norm, padSize, 0, 'both');
+
+
+% winograd loop
 % first approach, mutiply everything in double then convert to fixed
-V = G * kernel * G.';
-U = B_T * input * B_T.';
+
+% size of the image is 640 * 480
+% for m=4,r=3
+m = 4;
+r = 3;  %default r = 3
+if (size_k == 0)
+    r = 3;
+elseif (size_k == 1)
+    r = 1;
+end
+
+%prepare the output matrix and the number of iters
+red_out_wino = fi(zeros(64, 64), 1, out_n, out_r);
+[height, width] = size(A_red);
+
+% performing winoPE for red channel
+for i = 1 : m : height
+    out_j = 1;
+    for j = 1 : m : width
+        
+        % hard coding 5 because m + r - 1 = 6
+        input = A_red_padded(i:i+5, j:j+5);  
+        
+        %fixed pint for kernel and input
+        input = double(fi(input, 1, input_n, input_r));
+        kernel_norm = double(fi(kernel_norm, 1, input_n, input_r));
+        
+        V = double(G * kernel_norm * G.');   %This line can be outside of the loop
+        U = double(B_T * input * B_T.');
+        
+        [out_U, out_V, Y] = winoPE(U, V, size_k, UV_n, UV_r, middle_n, middle_r, out_n, out_r);
+        red_out_wino(i:i+3, j:j+3) = Y;
+
+    end
+end
+
+
+
+diff_float = red_out_wino - red_float_out
+max_diff_wino = max(max(abs(diff_float)))
+
+diff_fixed = red_out_wino - red_out_fixed;
+max_diff_wino_fixed = max(max(abs(diff_fixed)));
+
+diff_float_fix_truth = red_float_out - red_out_fixed;
+max_diff_truths = max(max(abs(diff_float_fix_truth)));
+
+
+%------------- END OF WINOPE TESTBENCH -------------------
+
+
+
+
+
+
+
 
 
