7376fceab4beaf8c2c43f33cf756da4db3ff9873
taoran-ji <158522125+taoran-ji@users.noreply.github.com>
2024-11-15 17:58:07 -0500

    Update data_top.sv

--- verilog/data_top.sv
+++ verilog/data_top.sv
@@ -23,11 +23,10 @@ module data_controller (
     // output to the memory
     output logic [15:0] input_addr_x_o,
     output logic [15:0] input_addr_y_o,
-    output logic [1:0] prefetch_type,         // 00 for initialize, 01 for two colomns, 10 for one colomn (boundary), 11 for one row
     output logic input_request_o,
 
     // input from the memory 
-    input logic signed [15:0] input_data_i[11:0],
+    input logic signed [15:0] input_data_i[5:0][5:0],
     input logic input_valid_i,
 
     // output to the PE arrays
@@ -35,18 +34,9 @@ module data_controller (
     output logic signed [15:0] result_tile_o_2 [5:0][5:0]
 );
 
-    // definition of the local reg to store off-chip input
-    logic signed [15:0] input_buffer [5:0][6:0];
-    logic direction;         // 0 for going right, 1 for going left
-    logic [2:0] pointer_x;
-    logic [2:0] pointer_y;
-    logic [1:0] prefetch_cnt;
-
-
-
-    
+    // definition of the local reg to store off-chip input    
     logic [7:0] total_id_reg;
-    logic size_type_reg;
+    logic size_type_reg, input_sel;
     logic [15:0] input_length_reg;
     logic [15:0] input_width_reg;
 
@@ -134,67 +124,147 @@ module data_controller (
         endcase
     end
 
+    logic signed [15:0] input_raw_1[5:0][5:0];
+    logic signed [15:0] input_raw_2[5:0][5:0];
+    
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
-            direction <= 0;
-            pointer_x <= 0;
-            pointer_y <= 0;
             buffer_ready <= 0;
             input_calculated <= 0;
-            prefetch_type <= 2'b00;
             input_addr_x_o <= 0;
             input_addr_y_o <= 0;
             prefetch_cnt <= 0;
+            input_sel <= 0;
         end else begin
-            if (prefetch_type == 2'b00 && weight_state == PREPARE) begin
-                if (prefetch_cnt < 3) begin
-                    prefetch_cnt <= prefetch_cnt + 1;
-                    input_addr_y_o <= input_addr_y_o + 2;
-                    buffer_ready <= 0;
-                end
-                else begin
-                    prefetch_cnt <= 0;
-                    input_addr_y_o <= input_addr_y_o + 1;
-                    prefetch_type <= 2'b10;
-                    buffer_ready <= 0;
-                end
+            if (weight_valid_i && weight_state == PREPARE) begin
+                if (input_sel == 0) input_raw_1 <= input_data_i;
+                else input_raw_2 <= input_data_i;
+                buffer_ready <= 1'b1;
+                input_sel <= ~input_sel;
             end
-            if (prefetch_type == 2'b10 && weight_state == PREPARE) buffer_ready <= 1;
+            else buffer_ready <= 1'b0;
 
-            if (input_valid_i) begin
-                case (prefetch_type)
-                    2'b00: begin
-                        input_buffer[0:5][pointer_y] <= input_data_i[0:5];
-                        input_buffer[0:5][pointer_y+1] <= input_data_i[6:11];
-                        pointer_y <= pointer_y + 2;
-                    end
-                    2'b01: begin
-                        if (pointer_y < 6) begin
-                            input_buffer[0:5][pointer_y] <= input_data_i[0:5];
-                            input_buffer[0:5][pointer_y+1] <= input_data_i[6:11];
-                            pointer_y <= (pointer_y == 5) ? 0 : pointer_y + 2;
-                        end
-                        else begin
-                            input_buffer[0:5][6] <= input_data_i[0:5];
-                            input_buffer[0:5][0] <= input_data_i[6:11];
-                            pointer_y <= 1;
-                        end
-                    end
-                    2'b10: begin
-                        input_buffer[0:5][pointer_y] <= input_data_i[0:5];
-                        pointer_y <= (pointer_y == 6) ? 0 : pointer_y + 1;
-                    end
-                    2'b11: begin
-                        input_buffer[pointer_x][0:6] <= input_data_i[0:6];
-                        pointer_x <= (pointer_x == 5) ? 0 : pointer_x + 1;
-                    end
-                endcase
-            end
-
-            
-
-            if (weight_state == START) input_calculated <= 1;
+            if (input_state == START) input_calculated <= 1;
             else input_calculated <= 0;
+
+            if (size_type_i) begin
+                if (input_addr_y_o + 4 < input_length_reg) 
+                    input_addr_y_o <= input_addr_y_o + 4;
+                else if (input_addr_x_o + 4 < input_width_reg) begin
+                    input_addr_y_o <= 0;
+                    input_addr_x_o <= input_addr_x_o + 4;
+                end
+                else begin 
+                    input_addr_y_o <= 0;
+                    input_addr_x_o <= 0;
+                end
+            end
+            else begin
+                if (input_addr_y_o + 6 < input_length_reg) 
+                    input_addr_y_o <= input_addr_y_o + 6;
+                else if (input_addr_x_o + 6 < input_width_reg) begin
+                    input_addr_y_o <= 0;
+                    input_addr_x_o <= input_addr_x_o + 6;
+                end
+                else begin 
+                    input_addr_y_o <= 0;
+                    input_addr_x_o <= 0;
+                end
+            end
+        end
+    end
+
+
+
+    logic signed [15:0] intermediate_result_1 [0:5][0:5];
+    logic signed [15:0] intermediate_result_2 [0:5][0:5];
+    logic signed [2:0] bt [0:5][0:5];
+    
+    assign bt[0][0] = 'd3;
+    assign bt[0][1] = 'd0;
+    assign bt[0][2] = -'d4;
+    assign bt[0][3] = 'd0;
+    assign bt[0][4] = 'd1;
+    assign bt[0][5] = 'd0;
+    
+    assign bt[1][0] = 'd0;
+    assign bt[1][1] = -'d3;
+    assign bt[1][2] = -'d3;
+    assign bt[1][3] = 'd1;
+    assign bt[1][4] = 'd1;
+    assign bt[1][5] = 'd0;
+    
+    assign bt[2][0] = 'd0;
+    assign bt[2][1] = 'd3;
+    assign bt[2][2] = -'d3;
+    assign bt[2][3] = -'d1;
+    assign bt[2][4] = 'd1;
+    assign bt[2][5] = 'd0;
+    
+    assign bt[3][0] = 'd0;
+    assign bt[3][1] = -'d2;
+    assign bt[3][2] = -'d1;
+    assign bt[3][3] = 'd2;
+    assign bt[3][4] = 'd1;
+    assign bt[3][5] = 'd0;
+    
+    assign bt[4][0] = 'd0;
+    assign bt[4][1] = 'd2;
+    assign bt[4][2] = -'d1;
+    assign bt[4][3] = -'d2;
+    assign bt[4][4] = 'd1;
+    assign bt[4][5] = 'd0;
+    
+    assign bt[5][0] = 'd0;
+    assign bt[5][1] = 'd3;
+    assign bt[5][2] = 'd0;
+    assign bt[5][3] = -'d4;
+    assign bt[5][4] = 'd0;
+    assign bt[5][5] = 'd1;
+
+    always_comb begin
+        for (int i = 0; i < 6; i=i+1) begin
+            for (int j = 0; j < 6; j=j+1) begin
+                intermediate_result_1[i][j] = 0;
+                intermediate_result_2[i][j] = 0;
+                for (int k = 0; k < 6; k=k+1) begin
+                    if (bt[i][k] == -'d4) begin
+                        intermediate_result_1[i][j] = intermediate_result_1[i][j] - (input_raw_1[k][j] <<< 2) - input_raw_1[k][j];
+                        intermediate_result_2[i][j] = intermediate_result_2[i][j] - (input_raw_2[k][j] <<< 2) - input_raw_2[k][j];
+                    end
+                    else if (bt[i][k] < 0) begin
+                        intermediate_result_1[i][j] = intermediate_result_1[i][j] - (input_raw_1[k][j] <<< (-bt[i][k] - 1));
+                        intermediate_result_2[i][j] = intermediate_result_2[i][j] - (input_raw_2[k][j] <<< (-bt[i][k] - 1));
+                    end
+                    else begin
+                        intermediate_result_1[i][j] = intermediate_result_1[i][j] + (input_raw_1[k][j] <<< (bt[i][k] - 1));
+                        intermediate_result_2[i][j] = intermediate_result_2[i][j] + (input_raw_2[k][j] <<< (bt[i][k] - 1));
+                    end
+                end
+            end
+        end
+    end
+
+    always_comb begin
+        for (int i = 0; i < 6; i=i+1) begin
+            for (int j = 0; j < 6; j=j+1) begin
+                result_tile_o_1[i][j] = 0;
+                result_tile_o_2[i][j] = 0;
+                for (int k = 0; k < 6; k=k+1) begin
+                    if (bt[j][k] == -'d4) begin
+                        result_tile_o_1[i][j] = result_tile_o_1[i][j] - (intermediate_result_1[i][k] <<< 2) - intermediate_result_1[i][k];
+                        result_tile_o_2[i][j] = result_tile_o_2[i][j] - (intermediate_result_2[i][k] <<< 2) - intermediate_result_2[i][k];
+                    end
+                    else if (bt[j][k] < 0) begin
+                        result_tile_o_1[i][j] = result_tile_o_1[i][j] - (intermediate_result_1[i][k] <<< (-bt[j][k] - 1));
+                        result_tile_o_2[i][j] = result_tile_o_2[i][j] - (intermediate_result_2[i][k] <<< (-bt[j][k] - 1));
+                    end
+                    else begin
+                        result_tile_o_1[i][j] = result_tile_o_1[i][j] + (intermediate_result_1[i][k] <<< (bt[j][k] - 1));
+                        result_tile_o_2[i][j] = result_tile_o_2[i][j] + (intermediate_result_2[i][k] <<< (bt[j][k] - 1));
+                    end
+                end
+            end
         end
     end
 
