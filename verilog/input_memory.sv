fde211fa2f0b5ab7cc79444fb87b9c0e832c9687
Wenjie_Geng <wenjieg@umich.edu>
2024-11-25 14:26:46 -0500

    IO for input_sram

--- verilog/input_memory.sv
+++ verilog/input_memory.sv
@@ -10,15 +10,16 @@ module data_mem_top (
     // if scan_mode is high, load the scan_in to the memory, only use one port
     // if scan_mode is low, work as the normal memory, dual port
 
-    // inpus from the controllers
+    // inputs from the controllers
     input logic [7:0] addr_1_in,
     input logic [7:0] addr_2_in,
     input logic addr_1_valid_in,
     input logic addr_2_valid_in,
 
-
-    output logic [511:0] data_out_1,
-    output logic [511:0] data_out_2,
+    output logic [511:0] data_1_out,
+    output logic [511:0] data_2_out,
+    output logic [7:0] addr_1_out, // use for debug
+    output logic [7:0] addr_2_out, // use for debug
     output logic data_1_valid_out,
     output logic data_2_valid_out,
 
@@ -50,8 +51,8 @@ module data_mem_top (
 
     // wires need to change later
     SRAM data_SRAM(
-        .CE1(sram_CEN_1),
-        .CE2(sram_CEN_2),
+        .CE1(clk),
+        .CE2(clk),
         .WEB1(sram_WEN_1),
         .WEB2(sram_WEN_2),
         .OEB1(1'b0),
@@ -62,6 +63,7 @@ module data_mem_top (
         .A2(sram_addr_2),
         .I1(sram_d_1),
         .I2(sram_d_2),
+        // I is only useful when scan_in
         .O1(sram_q_1),
         .O2(sram_q_2),
     );
@@ -70,4 +72,7 @@ module data_mem_top (
 endmodule
 
 
-
+// CE is just the clk
+// CSB should be low, otherwise the data and address inputs are disabled
+// OEB should be low, otherwise the output will be z state
+// WEB = 0, write enable, WEB = 1, read enable
