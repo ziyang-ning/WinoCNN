f1303183f01f94be0e5aeadb8fcb09933f8a1029
Wenjie_Geng <wenjieg@umich.edu>
2024-11-26 15:19:19 -0500

    finish input_memory

--- verilog/input_memory.sv
+++ verilog/input_memory.sv
@@ -7,8 +7,11 @@ module data_mem_top (
     input logic [511:0] scan_in,
     input logic scan_enable,
     input logic scan_mode,
+    input logic [7:0] scan_addr, 
     // if scan_mode is high, load the scan_in to the memory, only use one port
     // if scan_mode is low, work as the normal memory, dual port
+    // scan_addr could be the input from the testbench
+    // no need to write a local counter in data_mem_top
 
     // inputs from the controllers
     input logic [7:0] addr_1_in,
@@ -24,32 +27,38 @@ module data_mem_top (
     output logic data_2_valid_out,
 
 );
-    // if scan_enable is high, load the scan_in to the memory, only use one port
-    // use a counter to get the addr that we want to load the scan_in
-    logic [7:0] scan_addr; // counter for the addr
-    always_ff @(posedge clk) begin
-        if (reset || ~scan_enable) begin    
-            scan_addr <= 7'b0;
-        end
-        else if (~reset && scan_enable) begin
-            scan_addr <= scan_addr + 7'b1;
-        end
-    end
-
 
     // declare the SRAM wires
-    logic sram_CEN_1;
-    logic sram_CEN_2;
     logic sram_WEN_1;
     logic sram_WEN_2;
     logic [6:0] sram_addr_1;
     logic [6:0] sram_addr_2;
-    logic [511:0] sram_d_1;
-    logic [511:0] sram_d_2;
-    logic [511:0] sram_q_1;
-    logic [511:0] sram_q_2;
+    logic [511:0] sram_input_1;
+    logic [511:0] sram_input_2;
+    logic [511:0] sram_output_1; // output
+    logic [511:0] sram_output_2; // output
 
-    // wires need to change later
+    always_comb begin 
+        sram_WEN_1 = scan_mode;
+        sram_WEN_2 = 0; // never write to the second port
+        sram_addr_1 = scan_mode ? scan_addr: addr_1_in;
+        sram_addr_2 = scan_mode ? scan_addr: addr_2_in;
+        sram_input_1 = scan_mode ? scan_in: sram_input_1;
+        sram_input_2 = scan_mode ? scan_in: sram_input_2;
+
+        // bypass signal, because sram read is a comb logic
+        data_1_valid_out = addr_1_valid_in;
+        data_2_valid_out = addr_2_valid_in;
+        data_1_out = sram_output_1;
+        data_2_out = sram_output_2;
+        addr_1_out = addr_1_in;
+        addr_2_out = addr_2_in;
+    end
+
+    // CE is just the clk
+    // CSB should be low, otherwise the data and address inputs are disabled
+    // OEB should be low, otherwise the output will be z state
+    // WEB = 0, write enable, WEB = 1, read enable
     SRAM data_SRAM(
         .CE1(clk),
         .CE2(clk),
@@ -61,18 +70,12 @@ module data_mem_top (
         .CSB2(1'b0),
         .A1(sram_addr_1),
         .A2(sram_addr_2),
-        .I1(sram_d_1),
-        .I2(sram_d_2),
-        // I is only useful when scan_in
-        .O1(sram_q_1),
-        .O2(sram_q_2),
+        .I1(sram_input_1),
+        .I2(sram_input_2),
+        .O1(sram_output_1),
+        .O2(sram_output_2),
     );
 
 
 endmodule
 
-
-// CE is just the clk
-// CSB should be low, otherwise the data and address inputs are disabled
-// OEB should be low, otherwise the output will be z state
-// WEB = 0, write enable, WEB = 1, read enable
