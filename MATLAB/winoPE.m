9cb8b26f8bab586bd3583a8011d7ea426eff4998
ZiyangNing <callmejasonbro@gmail.com>
2024-11-30 14:41:26 -0500

    changes to MATLAB, and reorg test files, generated more tests

--- MATLAB/winoPE.m
+++ MATLAB/winoPE.m
@@ -11,6 +11,9 @@ function [out_U, out_V, Y] = winoPE(in_U, in_V, size_k, U_n, U_r, V_n, V_r, ...
 %     in_U = double(fi(in_U, 1, U_n, U_r));
 %     in_V = double(fi(in_V, 1, 12, 11));
 
+F = fimath('RoundingMethod','Floor');
+
+
 % new approach
     in_U = fi(in_U, 1, U_n, U_r);
     in_V = fi(in_V, 1, V_n, V_r);
@@ -31,24 +34,32 @@ function [out_U, out_V, Y] = winoPE(in_U, in_V, size_k, U_n, U_r, V_n, V_r, ...
     A_T = [1, 1, 1, 1, 1, 0;
            0, 1,-1, 2, -2, s0;
            0, 1, 1, 4, 4, 0;
-           0, 1, -1,8,-8, s1];
+           0, 1, -1,8,-8, s1;
+           0, 1, 1, 16, 16, 0;
+           0, 1, -1, 32, -32, 1];
        
-    if (size_k == 1)
-        A_T = A_T(1:2, :);
+    if (size_k == 0)
+        A_T = A_T(1:4, :);
     end
        
-   A_T = double(fi(A_T, 1, middle_n, middle_r));
+%    A_T = double(fi(A_T, 1, middle_n, middle_r));
    
    
    
 %    UV = double(fi(in_U .* in_V, 1, middle_n, middle_r));
    UV = in_U .* in_V;
-   UV = double(fi(UV, 1, middle_n, middle_r));
+   UV = fi(UV, 1, middle_n, middle_r, F);
+   UV_d = double(UV);
    
-   Y = A_T * UV * A_T.';
-   Y = fi(Y, 1, Y_n, Y_r);
    
-   if(file_generation == 1)
+   Y_inter = A_T * UV_d;
+   Y_inter = fi(Y_inter, 1, middle_n, middle_r, F);
+   Y_inter_d = double(Y_inter);
+   
+   Y = Y_inter_d * A_T.';
+   Y = fi(Y, 1, Y_n, Y_r, F);
+   
+   if(file_generation >= 1)
        writematrix(Y.int, fullfile(output_folder_name, strcat(string(filename_count) ,'output.txt')), 'Delimiter', ' ');
    end
    
--- MATLAB/winoPE_test.m
+++ MATLAB/winoPE_test.m
@@ -5,13 +5,80 @@ clc;
 
 %------------- Setup Params -------------------
 
-size_k = 0;       % size_k = 0 for 3*3, = 1 for 1*1
+% extract data from image
+A = imread('2424_pic.jpg');
+
+% input and output size
+input_n = 8;    %size for the input matrix and filter
+input_r = 7;
+U_n = 14;
+U_r = 7;
+V_n = 12;
+V_r = 11;
+middle_n = 16;
+middle_r = 11;
+out_n = 12;
+out_r = 7;
+
+size_k = 1;       % size_k = 0 for 3*3, = 1 for 1*1
+
+% if file_generation = 1 then U, V matrices to txt files for 3*3
+% if file_generation = 2 then U, V matrices to txt files for 1*1
+% if file_generation = 3 then write input (8 bit sliced image) and V
+% to txt files for memory tests
+% NOTE:: setting file_generation param will automatically change size_k
+
+file_generation = 3;
+
+if (file_generation == 1)
+    size_k = 0;
+    input_folder_name = fullfile('..', 'matlab_data_out/3by3UV');
+    if ~exist(input_folder_name, 'dir')
+        mkdir(input_folder_name);
+    end
+
+    output_folder_name = fullfile('..', 'matlab_data_out/ans_33');
+    if ~exist(output_folder_name, 'dir')
+        mkdir(output_folder_name);
+    end
+
+elseif(file_generation == 2)
+    size_k = 1;
+    input_folder_name = fullfile('..', 'matlab_data_out/1by1UV');
+    if ~exist(input_folder_name, 'dir')
+        mkdir(input_folder_name);
+    end
+
+    output_folder_name = fullfile('..', 'matlab_data_out/ans_11');
+    if ~exist(output_folder_name, 'dir')
+        mkdir(output_folder_name);
+    end
+    
+else %file_generation == 3
+    size_k = 0;
+    input_folder_name = fullfile('..', 'matlab_data_out/input_filter_HEX');
+    input_HEX_fileName = fullfile(input_folder_name, 'input.txt');
+    filter_HEX_fileName = fullfile(input_folder_name, 'filter.txt');
+
+    input_HEX_fileID = fopen(input_HEX_fileName, 'w');
+    filter_HEX_fileID = fopen(filter_HEX_fileName, 'w');
+    
+    if ~exist(input_folder_name, 'dir')
+        mkdir(input_folder_name);
+    end
+
+    output_folder_name = fullfile('..', 'matlab_data_out/ans_33inputTEST');
+    if ~exist(output_folder_name, 'dir')
+        mkdir(output_folder_name);
+    end
+end
+
 
 % should be values from -1 to 1?
 if (size_k == 0)
     % TODO: CHANGE YOUR kernel HERE
     % NOTE: the kernel will be later normalized to range from [-1 to 1]
-    kernel_1 =    [-1, -1, -1; 
+    kernel =    [-1, -1, -1; 
                  -1,  2, -1;    % was 8 in the middle
                  -1, -1, -1];   % Edge detection kernel
              
@@ -19,7 +86,7 @@ if (size_k == 0)
                    -1, 5, -1;    % Sharpen
                    0, -1, 0];
                
-    kernel =       [1, 0, -1; 
+    kernel_3 =       [1, 0, -1; 
                    2, 0, -2;    % random
                    1, 0, -1];
                
@@ -42,7 +109,7 @@ if (size_k == 0)
     s1 = 1;
 elseif (size_k == 1)
      % TODO: CHANGE YOUR kernel HERE
-     kernel = [0.3];
+     kernel = 1;
      s0 = 1;
      s1 = 0;
 end
@@ -65,33 +132,8 @@ B_T = [4, 0, -5, 0, 1, 0;
 if (size_k == 1)
     G = G(:, 1);
 end
-        
-% input and output size
-input_n = 8;    %size for the input matrix and filter
-input_r = 7;
-U_n = 14;
-U_r = 7;
-V_n = 12;
-V_r = 11;
-middle_n = 16;
-middle_r = 11;
-out_n = 12;
-out_r = 7;
 
-% if file_generation = 1 then write input and weight matrix to txt files
-file_generation = 1;
-input_folder_name = fullfile('..', 'matlab_data_out');
-if ~exist(input_folder_name, 'dir')
-    mkdir(input_folder_name);
-end
 
-output_folder_name = fullfile('..', 'matlab_data_out/ans');
-if ~exist(output_folder_name, 'dir')
-    mkdir(output_folder_name);
-end
-
-% extract data from image
-A = imread('test_pic_1.jpg');
 %
 % A = ones(64,64,3);  %All white image
 
@@ -106,8 +148,15 @@ A_green_norm = double(A_green) / 255;
 A_blue_norm = double(A_blue) / 255;
 
 % normalize kernel to [-1, 1]
-max_val = max(abs(kernel(:)));
-kernel_norm = kernel / max_val;
+
+if(size_k == 0)
+    max_val = max(abs(kernel(:)));
+    kernel_norm = kernel / max_val;
+else
+    kernel_norm = kernel;
+end
+
+
 
 %------------- END OF Setup Params -------------------
 
@@ -163,21 +212,20 @@ A_blue_padded = padarray(A_blue_norm, padSize, 0, 'both');
 
 
 % winograd loop
-% first approach, mutiply everything in double then convert to fixed
-
-% size of the image is 640 * 480
-% for m=4,r=3
+% default m=4,r=3
 m = 4;
-r = 3;  %default r = 3
+r = 3;
 if (size_k == 0)
     r = 3;
+    m = 4;
 elseif (size_k == 1)
     r = 1;
+    m = 6;
 end
 
 %prepare the output matrix and the number of iters
-red_out_wino = zeros(64, 64);
 [height, width] = size(A_red);
+red_out_wino = zeros(height, width);
 
 % filename counter for test data output
 filename_count = 0;
@@ -188,9 +236,16 @@ for i = 1 : m : height
     for j = 1 : m : width
         
         % hard coding 5 because m + r - 1 = 6
-        input = A_red_padded(i:i+5, j:j+5);  
+        input = A_red_padded(i:i+5, j:j+5);
         
-        %fixed pint for kernel and input
+        if(file_generation == 3)
+            input_HEX_vals = fi(input, 1, input_n, input_r).hex;
+            flattened = strjoin(string(input_HEX_vals), ' ');
+            pretty_HEX_data = regexprep(flattened, '\s+', ' '); % Replace multiple spaces with a single space
+            fprintf(input_HEX_fileID, '%s\n', pretty_HEX_data); % Write the character array
+        end
+        
+        %fixed point for kernel and input
         input = double(fi(input, 1, input_n, input_r));
         
         % do kernel calc in floating point
@@ -199,7 +254,7 @@ for i = 1 : m : height
         V = double(G * kernel_norm * G.');
         U = double(B_T * input * B_T.');
         
-        if(file_generation == 1)
+        if(file_generation == 1 || file_generation == 2)
             in_U = double(fi(U, 1, U_n, U_r).int);
             in_V = double(fi(V, 1, V_n, V_r).int);
 
@@ -208,11 +263,24 @@ for i = 1 : m : height
             writematrix(in_V, fullfile(input_folder_name, 'in_V.txt'), 'Delimiter', ' ');
             filename_count = filename_count + 1;
         end
+        if(file_generation == 3 && filename_count == 0)
+            in_V_HEX = fi(V, 1, V_n, V_r).hex;
+            V_flattened = strjoin(string(in_V_HEX), ' ');
+            pretty_V_HEX_data = regexprep(V_flattened, '\s+', ' '); % Replace multiple spaces with a single space
+            fprintf(filter_HEX_fileID, '%s\n', pretty_V_HEX_data); % Write the character array
+            fclose(filter_HEX_fileID);
+            filename_count = filename_count + 1;
+        end
         
         [out_U, out_V, Y] = winoPE(U, V, size_k, U_n, U_r, V_n, V_r, middle_n, middle_r, out_n, out_r, ...
                                  file_generation, output_folder_name, filename_count);
+        if(size_k == 0)
+            red_out_wino(i:i+3, j:j+3) = Y;
+        end
         
-        red_out_wino(i:i+3, j:j+3) = Y;
+        if(size_k == 1)
+            red_out_wino(i:i+5, j:j+5) = Y;
+        end
 
     end
 end
