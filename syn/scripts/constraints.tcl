0a7a570c4c2e55c35bce6fd9c638ba122cf1bc39
Wenjie_Geng <wenjieg@umich.edu>
2024-12-06 17:25:42 -0500

    pass syn

--- syn/scripts/constraints.tcl
+++ syn/scripts/constraints.tcl
@@ -2,8 +2,8 @@ set_max_transition 0.150 ${DESIGN_NAME}
 set_input_transition 0.080 [all_inputs]
 set_max_transition 0.080 [all_outputs]
 
-set clock_period 10
-set clock_uncertainty [expr $clock_period * 0.10]
+set clock_period 12
+set clock_uncertainty [expr $clock_period * 0.01]
 set clock_transition 0.080
 set clock_latency 0.2
 
